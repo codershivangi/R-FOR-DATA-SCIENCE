4b6fe25eaf6192a1a504623cb4bfae4569ce83d0
Shivangi <159949628+codershivangi@users.noreply.github.com>
2025-02-01 09:11:50 -0800

    Add files via upload

---
+++ Lab_Programs/Unit_1/lab_1_6.R
@@ -0,0 +1,10 @@
+# Extract the first and last elements of already created vector from,storing them as a new object
+
+v <-c(2,3,4,5,6)
+print(v)
+first_ele <- v[1]
+print(first_ele)
+last_ele<- v[length(v)]
+print(last_ele)
+
+
