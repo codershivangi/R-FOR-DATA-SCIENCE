748ddbf75f0c33f291de29908aba26c105b72500
Shivangi <159949628+codershivangi@users.noreply.github.com>
2025-02-05 23:34:16 -0800

    Add files via upload

--- Lab_programs/lab_1_1.R
+++ Lab_programs/lab_1_1.R
@@ -1,1 +1,16 @@
+# Basic Mathematical Computations
 
+a = 12
+b = 81
+sum <- a + b
+diff <- a - b
+squre <- a^2
+squreroot <- sqrt(b)
+expo <- exp(a)
+logarithmic <- log(a,2)
+print(sum)
+print(diff)
+print(squre)
+print(squreroot)
+print(expo)
+print(logarithmic)
