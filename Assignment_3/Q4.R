8fde0bdd39ced095a79157deca47a90bf1469d35
Shivangi <159949628+codershivangi@users.noreply.github.com>
2025-03-16 09:44:20 -0700

    Update Q4.R

--- Assignment_3/Q4.R
+++ Assignment_3/Q4.R
@@ -1,7 +1,6 @@
 logical_vector<-rep(c(TRUE,FALSE,TRUE,FALSE),length.out=12)
 logical_matrix<-matrix(logical_vector,nrow=3,ncol=4,byrow=TRUE)
 logical_vector<-rep(c(TRUE,FALSE,TRUE,FALSE),length.out=12)
-print("Logical Matrix:")
 logical_matrix
 numeric_matrix<-matrix(c(1,0,1,0,1,2,3,4,0,1,0,1),nrow=3,ncol=4,byrow=TRUE)
 numeric_matrix
