261703e5757b2c8bec3775cc82cb8d02bce1dcc2
Shivangi <159949628+codershivangi@users.noreply.github.com>
2025-04-08 03:29:05 -0700

    Delete Lab_Programs/Unit-2 directory

--- Lab_Programs/Unit-2/Lab_2_3.R
+++
@@ -1,3 +0,0 @@
-#Use a fourfold repetition of the second row of the matrix formed in (2) to fill a new array of dimensions 2*2*2*2
-new_array <- array(rep(extract_matrix[2,],times=4),dim=c(2,2,2,2))
-print(new_array)
--- Lab_Programs/Unit-2/Lab_2_4.R
+++
@@ -1,3 +0,0 @@
-#Create a new array comprised of the results of deleting the sixth layer of (1)
-array_modified <- array_3D[, ,-6]
-print(array_modified)
