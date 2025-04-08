a789f1ca365f553a3db9239ddb8e20e12053bd76
Shivangi <159949628+codershivangi@users.noreply.github.com>
2025-04-08 03:29:28 -0700

    Delete Lab_Programs/Unit_4 directory

--- Lab_Programs/Unit_4/lab_4_3.R
+++
@@ -1,3 +0,0 @@
-df$age <- c(25, 30, 28, 22)  
-result <- df[df$sex == "Female" & df$funny %in% c("MED", "HIGH"), c("person", "age")]
-print(result)
--- Lab_Programs/Unit_4/lab_4_4.R
+++
@@ -1,2 +0,0 @@
-s_names <- df[grep("^S", df$person), ]
-print(s_names)
