914b1cfc5f6bdf763abcd2008a5ddaeee1e0168b
Shivangi <159949628+codershivangi@users.noreply.github.com>
2025-04-08 03:28:55 -0700

    Delete Lab_Programs/Unit_1 directory

--- Lab_Programs/Unit_1/lab_1_2.R
+++
@@ -1,6 +0,0 @@
-# Create an object X that stores the value then overwrite the object in by itself divided by Y.Print the result to the console.
-
-X <- 4
-Y <- 2
-X <- X/Y
-print(X)
--- Lab_Programs/Unit_1/lab_1_5.R
+++
@@ -1,3 +0,0 @@
-# Confirm that the length of the vector created is 20
-v <- seq(1:20)
-print(length(v))
