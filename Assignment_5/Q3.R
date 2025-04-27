0c8c345052907bc15c0a9e5a4d41baf80b968808
Shivangi <159949628+codershivangi@users.noreply.github.com>
2025-04-27 04:20:34 -0700

    Update Q3.R

--- Assignment_5/Q3.R
+++ Assignment_5/Q3.R
@@ -1,3 +1,13 @@
-jpeg("plot.jpeg")
-qplot(x,y)
+library(ggplot2)
+
+x <- c(1, 2, 3, 4, 5)
+y <- c(2, 4, 1, 7, 6)
+
+setwd("C:\Users\DELL\Documents\R Assignment_5")
+getwd()
+
+jpeg(filename = "scatter_plot.jpg")
+
+qplot(x, y, geom = "point", main = "Simple Scatter Plot", xlab = "X values", ylab = "Y values")
+
 dev.off()
