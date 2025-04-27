1de52996332aa883119e40d3b707733272eecfbf
Shivangi <159949628+codershivangi@users.noreply.github.com>
2025-04-27 04:24:57 -0700

    Update Q5.R

--- Assignment_5/Q5.R
+++ Assignment_5/Q5.R
@@ -5,4 +5,8 @@ y <- c(2, 4, 1, 7, 6)
 
 data <- data.frame(x, y)
 
-ggplot(data, aes(x = x, y = y)) + geom_line(color = "blue", linewidth = 1)
+ggplot(data, aes(x = x, y = y)) +
+  geom_line(color = "blue", size = 1) +
+  ggtitle("Simple Line Plot") +
+  xlab("X values") +
+  ylab("Y values")
