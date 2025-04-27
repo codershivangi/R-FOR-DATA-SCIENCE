7b75cb5e024a99e6b299ecc934ceb7691f7a61a7
Shivangi <159949628+codershivangi@users.noreply.github.com>
2025-04-27 04:22:19 -0700

    Update Q4.R

--- Assignment_5/Q4.R
+++ Assignment_5/Q4.R
@@ -1,5 +1,8 @@
-df<-data.frame(
-  x=c(1,2,3,4,5),
-  y=c(1,2,3,4,5)
-)
-ggplot(df,aes(x=x,y=y))+geom_line()
+library(ggplot2)
+
+x <- c(1, 2, 3, 4, 5)
+y <- c(2, 4, 1, 7, 6)
+
+data <- data.frame(x, y)
+
+ggplot(data, aes(x = x, y = y)) + geom_line(color = "blue", linewidth = 1)
