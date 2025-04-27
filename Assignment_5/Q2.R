761cdb13e85341ad0a9c0bac958e5ee7997f8910
Shivangi <159949628+codershivangi@users.noreply.github.com>
2025-04-27 04:13:16 -0700

    Update Q2.R

--- Assignment_5/Q2.R
+++ Assignment_5/Q2.R
@@ -1,3 +1,5 @@
-x<-c(1,2,3,4,5)
-y<-c(1,2,3,4,5)
-qplot(x,y)
+library("ggplot2")
+x <- c(1, 2, 3, 4, 5)
+y <- c(2, 4, 1, 7, 6)
+
+qplot(x, y, geom = "point", main = "Simple Scatter Plot", xlab = "X values", ylab = "Y values")
